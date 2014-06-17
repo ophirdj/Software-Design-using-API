db0249b9d44074973adb127b9868e791e319ea44
Ziv Ronen <akarks@gmail.com>
2014-06-17 19:45:44 +0300

    Add the tweet between finder

---
+++ twitter-knowledge-center/src/main/java/ac/il/technion/twc/impl/queries/TweetBetweenFinder.java
@@ -0,0 +1,77 @@
+package ac.il.technion.twc.impl.queries;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import ac.il.technion.twc.api.Tweet;
+import ac.il.technion.twc.api.TwitterQueryAPI;
+import ac.il.technion.twc.api.visitor.Visitor;
+
+/**
+ * @author Ziv Ronen
+ * @date 17.06.2014
+ * @mail akarks@gmail.com
+ */
+public class TweetBetweenFinder implements Visitor {
+
+  private static SimpleDateFormat QUERY_DATE_FORMAT = new SimpleDateFormat(
+      "dd/MM/yyyy HH:mm:ss");
+  private List<Tweet> tweets;
+
+  @Override
+  public void visit(final TwitterQueryAPI twitter) {
+    tweets = twitter.getTweets();
+  }
+
+  @Override
+  public void clearData() {
+    tweets.clear();
+  }
+
+  /**
+   * @param from
+   *          The start of the time interval
+   * @param to
+   *          The end of the time interval
+   * @return An id of tweet in the given interval
+   * @throws NotTweetInRangeException
+   *           If no such tweet exist
+   */
+  public String findTweetBetween(final Date from, final Date to)
+      throws NotTweetInRangeException {
+    for (final Tweet tweet : tweets)
+      if (from.before(tweet.getDate()) && to.after(tweet.getDate()))
+        return tweet.getTweetID();
+    throw new NotTweetInRangeException();
+  }
+
+  /**
+   * Exeption to throw if no tweet exists in the given range
+   * 
+   * @author Ziv Ronen
+   * @date 17.06.2014
+   * @mail akarks@gmail.com
+   */
+  public static class NotTweetInRangeException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6232645509562698747L;
+
+  }
+
+  /**
+   * @param str
+   *          string representing a date
+   * @return the date of the tweet
+   * @throws ParseException
+   *           if the string is not a valid date
+   */
+  public static Date fromString(final String str) throws ParseException {
+    return QUERY_DATE_FORMAT.parse(str);
+  }
+
+}
