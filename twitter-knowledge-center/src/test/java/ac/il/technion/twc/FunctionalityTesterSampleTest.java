e0387887b4b99ddb422ab03425fafb4e0235d428
Ziv Ronen <akarks@gmail.com>
2014-06-18 23:41:37 +0300

    Improve sample test

--- twitter-knowledge-center/src/test/java/ac/il/technion/twc/FunctionalityTesterSampleTest.java
+++ twitter-knowledge-center/src/test/java/ac/il/technion/twc/FunctionalityTesterSampleTest.java
@@ -30,12 +30,12 @@ public class FunctionalityTesterSampleTest {
   @Test
   public final void sampleTest1() throws Exception {
     $.importData(new String[] {
-        "04/04/2014 12:00:00, 1000", "05/04/2014 12:00:00, 1001, 1000" }); //$NON-NLS-1$ //$NON-NLS-2$
+        "04/04/2014 12:00:00, 1000", "05/04/2014 12:00:00, 1001, 1000", "06/04/2014 12:00:00, 1002, 1001" }); //$NON-NLS-1$ //$NON-NLS-2$
     $.setupIndex();
     assertEquals("1001",
         $.findTweetInTime("04/04/2014 12:00:01", "05/04/2014 12:00:01"));
     assertEquals("1000", $.getOriginalTweetsId("1001"));
-    assertEquals("1", $.numberOfRetweets("1000"));
+    assertEquals("2", $.numberOfRetweets("1000"));
     $.importDataJson(new String[] { JSONTweetsMessages.getString("Sample1"),
         JSONTweetsMessages.getString("Sample2"),
         JSONTweetsMessages.getString("Sample3") });
